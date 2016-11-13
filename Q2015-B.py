78f89b4bc9f8c712175b4d6f53c2dcdad78d907a
Pavlos Loukareas <ploukareas@gmail.com>
2016-11-13 13:17:02 +0200

    Add files via upload

---
+++ Q2015-B.py
@@ -0,0 +1,23 @@
+# Google Code Jam 2015 Qualification Round, problem 2
+
+txtfile = open("B-large-practice.in","r")
+output_file = open("large_output.txt","w")
+T = txtfile.readline()
+
+for i in range(int(T)):
+    D = txtfile.readline()
+    Diners = txtfile.readline()
+    diners = Diners.split()
+    diners = map(int,diners)
+    max_pan = max(diners)
+
+    for j in range(1, max(diners) + 1):
+        total_moves = 0
+        for k in diners:
+            total_moves += (k - 1) / j
+        max_pan = min(max_pan, total_moves + j)
+        
+    output_file.write("Case #" + str(i + 1) + ": " + str(max_pan) + '\n')
+
+output_file.close()
+txtfile.close()
