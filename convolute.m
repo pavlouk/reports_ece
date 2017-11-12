e668559f78fa9bfbd09a863e93510964d0adc2b7
Pavlos Loukareas <ploukareas@gmail.com>
2017-11-12 19:47:45 +0200

    Add files via upload

---
+++ convolute.m
@@ -0,0 +1,20 @@
+function [conv_sign, n] = convolute(sign_A, sign_B)
+%convolute.m Calculate the convolution of the discrete input singals 
+%   The output will return both the signal and its length
+
+N1 = length(sign_A);
+N2 = length(sign_B);
+
+extsign_A = [zeros(1, N2 - 1) sign_A zeros(1, N2 - 1)];
+conv_sign = zeros(1, N1 + N2 - 1);
+for m=1:N1 + N2 - 1
+    cval = 0;
+    for n=1:N2
+        cval = cval + extsign_A(m + n -1)*sign_B(N2 - n + 1);
+    end
+    conv_sign(m) = cval;
+end
+n = N1 + N2 - 1;
+
+end
+
