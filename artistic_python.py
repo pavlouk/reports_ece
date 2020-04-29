4c032ce1ca75741ba4d11f2649816966f4c26e36
19766015 <ploukareas@gmail.com>
2020-04-30 00:29:59 +0300

    changed formatting

--- artistic_python.py
+++ artistic_python.py
@@ -22,15 +22,19 @@ class Window:
         self.root.config(menu=self.gameMenu)
         self.fileMenu = Menu(self.gameMenu)
         self.gameMenu.add_cascade(label="File", menu=self.fileMenu)
-        self.fileMenu.add_command(label="Connect all nodes", command=self.connect)
-        self.fileMenu.add_command(label="Color all lines", command=self.setlineColor)
+        self.fileMenu.add_command(
+            label="Connect all nodes", command=self.connect)
+        self.fileMenu.add_command(
+            label="Color all lines", command=self.setlineColor)
         self.fileMenu.add_command(label="New", command=self.new)
-        self.fileMenu.add_command(label="Create Triangles", command=self.createTriangles)
+        self.fileMenu.add_command(
+            label="Create Triangles", command=self.createTriangles)
         self.fileMenu.add_command(label="Exit", command=self.root.destroy)
         self.root.bind("<Button-1>", self.point)
 
     def point(self, event):
-        self.gameCanvas.create_oval(event.x - 5, event.y - 5, event.x + 5, event.y + 5, fill="white")
+        self.gameCanvas.create_oval(
+            event.x - 5, event.y - 5, event.x + 5, event.y + 5, fill="white")
         self.setofPoints.append(Node(event.x, event.y))
         self.root.mainloop()
 
@@ -63,7 +67,8 @@ class Window:
                 elif len(B) == 1:
                     B = hex(randint(0, 15))[2:] + B
                 self.color = "#" + R + G + B
-                self.gameCanvas.create_line(i.x, i.y, j.x, j.y, fill=self.color)
+                self.gameCanvas.create_line(
+                    i.x, i.y, j.x, j.y, fill=self.color)
 
     def createTriangles(self):
         self.gameCanvas.delete(ALL)
@@ -71,13 +76,15 @@ class Window:
             distances = []
             for j in self.setofPoints:
                 if i != j:
-                    distances.append([(sqrt((j.x - i.x)**2 + (j.y - i.y)**2)), j.x, j.y])
+                    distances.append(
+                        [(sqrt((j.x - i.x)**2 + (j.y - i.y)**2)), j.x, j.y])
             distances.sort(key=itemgetter(0))
             del distances[2:]
             self.distances += distances
         for i in range(0, len(self.setofPoints)):
             self.color = ""
-            R = hex((self.setofPoints[i].x + self.distances[2*i][1] + self.distances[2*i + 1][1])/3)[2:]
+            R = hex((self.setofPoints[i].x + self.distances[2*i]
+                     [1] + self.distances[2*i + 1][1])/3)[2:]
             if len(R) >= 3:
                 R = R[-2:]
             elif len(R) == 1:
@@ -88,14 +95,16 @@ class Window:
                 G = G[-2:]
             elif len(G) == 1:
                 G = hex(randint(0, 15))[2:] + G
-            B = hex((self.setofPoints[i].y + self.distances[2*i][2] + self.distances[2*i + 1][2])/3)[2:]
+            B = hex((self.setofPoints[i].y + self.distances[2*i]
+                     [2] + self.distances[2*i + 1][2])/3)[2:]
             if len(B) >= 3:
                 B = B[-2:]
             elif len(B) == 1:
                 B = hex(randint(0, 15))[2:] + B
             self.color = "#" + R + G + B
             self.gameCanvas.create_polygon(self.setofPoints[i].x, self.setofPoints[i].y, self.distances[2*i][1],
-                                           self.distances[2*i][2], self.distances[2*i + 1][1], self.distances[2*i + 1][2],
+                                           self.distances[2*i][2], self.distances[2 *
+                                                                                  i + 1][1], self.distances[2*i + 1][2],
                                            fill=self.color)
 
 
