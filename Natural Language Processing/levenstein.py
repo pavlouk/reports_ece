efd9933b7047c37d702ae516a5daf24a13ddbf49
Pavlos Loukareas <ploukareas@gmail.com>
2020-11-22 13:14:36 +0200

    made conventional changes

--- Natural Language Processing/levenstein.py
+++ Natural Language Processing/levenstein.py
@@ -1,12 +1,12 @@
-def Levenstein(S, T):
+def levenstein(S, T):
     
     if S == " ":
         return len(T)
     if T == " ":
         return len(S)
-    p1 = Levenstein(S, T[:-1]) + 1
-    p2 = Levenstein(S[:-1], T) + 1
-    p3 = Levenstein(S[:-1], T[:-1])
+    p1 = levenstein(S, T[:-1]) + 1
+    p2 = levenstein(S[:-1], T) + 1
+    p3 = levenstein(S[:-1], T[:-1])
     if S[-1] != T[-1]:
         p3 += 1
     
