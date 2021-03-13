e85a7b63095cd3d70573b02e8e45db38382c412d
Pavlos Loukareas <ploukareas@gmail.com>
2021-03-13 17:47:05 +0200

    setup file for package

--- src/data/initial_data_pick.py
+++ src/data/initial_data_pick.py
@@ -1,6 +1,17 @@
-import os 
+import os
 
+from pathlib import Path
 
+from .utils import get_project_root
 def initial_data_pick():
-    
-    os.path
+    """ 
+    αυτός ο κώδικας μεταφέρει όλα τα δεδομένα στο φάκελο data/interim
+    τα δεδομένα είναι:
+        DC_*.jpg
+        IR_*.jpg
+        CSV_*.csv
+
+         """
+    root_directory = get_project_root()
+    if os.path.exists(os.path.join(root_directory, 'data/raw')):
+        
