425a49742719a767265d10fff8d22dc96cd6613a
Pavlos Loukareas <ploukareas@gmail.com>
2021-03-27 18:16:12 +0200

    added visualization features

--- src/visualization/gif_maker.py
+++ src/visualization/gif_maker.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from skimage.exposure import histogram
+from skimage.util import img_as_ubyte
 
 class UpdateDist:
     def __init__(self, ax, list1):
@@ -41,4 +43,43 @@ def gif_maker(image_list1, image_list2):
     
     plt.show()
     
-    return None 
+    return None
+
+def cut_uncut_compare(rawImages, cutImages, markerBack, markerBody):
+    
+    for cutImage, rawImage in zip(cutImages, rawImages):
+        # -------------- Figure: 2-figure about the FLIR logo removal --------
+        fig = plt.figure(figsize=(8, 8), constrained_layout=False)
+        fig.suptitle(t='Border Removal', fontsize='xx-large')
+        grid = fig.add_gridspec(nrows=2, ncols=2, wspace=0.2, hspace=0.2)
+        
+        ax = fig.add_subplot(grid[0, 0])
+        ax.imshow(X=rawImage, cmap=plt.cm.nipy_spectral)
+        ax.axvline(x=100, c='red')
+        ax.axvline(x=260, c='red')
+        ax.set_title(label='Image')
+        ax.set_ylabel(ylabel='Raw Image')
+        fig.add_subplot(ax)
+        
+        ax = fig.add_subplot(grid[0, 1])
+        histRaw, histRaw_centers = histogram(img_as_ubyte(rawImage))
+        ax.plot(histRaw_centers, histRaw, lw=1)
+        ax.set_title(label='Histogram')
+        fig.add_subplot(ax)
+        
+        ax = fig.add_subplot(grid[1, 0])
+        ax.imshow(X=cutImage, cmap=plt.cm.nipy_spectral)
+        ax.set_ylabel(ylabel='Cut Image')
+        fig.add_subplot(ax)
+    
+        ax = fig.add_subplot(grid[1, 1])
+        histCut, histCut_centers = histogram(img_as_ubyte(cutImage))
+        ax.plot(histCut_centers, histCut, lw=1)
+        ax.axvline(x=markerBody, color='red', linestyle='--')
+        ax.axvline(x=markerBack, color='blue', linestyle='--')
+        ax.axvspan(xmin=markerBody, xmax=255, color='red', alpha=0.15)
+        ax.axvspan(xmin=0, xmax=markerBack, color='blue', alpha=0.15)
+        fig.add_subplot(ax)
+        
+def snr_print():
+    
