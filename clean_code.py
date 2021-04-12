d0944bddb92e52065954b43e729221b87461de67
Pavlos Loukareas <ploukareas@gmail.com>
2021-04-12 17:15:07 +0300

    master python file

--- clean_code.py
+++ clean_code.py
@@ -31,6 +31,12 @@ from sklearn.metrics import silhouette_score, calinski_harabasz_score, davies_bo
 from sklearn.feature_extraction import img_to_graph, grid_to_graph
 from scipy import ndimage as ndi
 
+dc_mask = np.zeros(dc_image.shape, dtype=np.uint8)
+from skimage import draw 
+rr, cc = draw.rectangle(start=(dc_image.shape[0]/2, dc_image.shape[1]/2), extent=(), shape=dc_image.shape)
+dc_mask[rr, cc] = 1.0
+new_image = dc_image * dc_mask 
+
 # Επεξεργασία του ποντικιού Άκης, επιλέγοντας τις καλύτερες —κατ'εμέ— εικόνες
 # Ποντίκια Άκης Γάκης Δάκης Λάκης Μάκης
 # basically we are building a data frame with all the columns made into a feature vector
@@ -53,81 +59,17 @@ sensorData = load_csv(sampleHours, name='BAT')
 # Display purposes: clear the FLIR logo and the pseudo-color scale
 # με το κεντράρισμα του ποντικιού ρίχνουμε το μέγεθος σε (240, 144)
 cutImages = [mouseImage[:, 100:260] for mouseImage in rawImages]
-
 gif_maker(rawImages, cutImages)
+from glob import glob
 
-for cutImage, rawImage in zip(cutImages, rawImages):
-    # -------------- Figure: 2-figure about the FLIR logo removal --------
-    fig = plt.figure(figsize=(8, 8), constrained_layout=False)
-    fig.suptitle(t='Border Removal', fontsize='xx-large')
-    grid = fig.add_gridspec(nrows=2, ncols=2, wspace=0.2, hspace=0.2)
-    
-    ax = fig.add_subplot(grid[0, 0])
-    ax.imshow(X=rawImage, cmap=plt.cm.nipy_spectral)
-    ax.axvline(x=100, c='red')
-    ax.axvline(x=260, c='red')
-    ax.set_title(label='Image')
-    ax.set_ylabel(ylabel='Raw Image')
-    fig.add_subplot(ax)
-    
-    ax = fig.add_subplot(grid[0, 1])
-    histRaw, histRaw_centers = histogram(img_as_ubyte(rawImage))
-    ax.plot(histRaw_centers, histRaw, lw=1)
-    ax.set_title(label='Histogram')
-    fig.add_subplot(ax)
-    
-    ax = fig.add_subplot(grid[1, 0])
-    ax.imshow(X=cutImage, cmap=plt.cm.nipy_spectral)
-    ax.set_ylabel(ylabel='Cut Image')
-    fig.add_subplot(ax)
+""" from src.visualization import gif_maker 
+    from gif_maker import cut_uncut_compare """
 
-    ax = fig.add_subplot(grid[1, 1])
-    histCut, histCut_centers = histogram(img_as_ubyte(cutImage))
-    ax.plot(histCut_centers, histCut, lw=1)
-    ax.axvline(x=markerBody, color='red', linestyle='--')
-    ax.axvline(x=markerBack, color='blue', linestyle='--')
-    ax.axvspan(xmin=markerBody, xmax=255, color='red', alpha=0.15)
-    ax.axvspan(xmin=0, xmax=markerBack, color='blue', alpha=0.15)
-    fig.add_subplot(ax)
+""" from src.features import downscale
+    from downscale import downscale """
 
-# Preprocessing -- στατιστικά εικόνας, πριν κάνουμε αλλαγές
-sigmaEstimatesBefore = [estimate_sigma(mouseImage) for mouseImage in cutImages]
-meanBefore = [np.mean(mouseImage) for mouseImage in cutImages]
-SNRBefore = list(map(lambda x, y: x / y, meanBefore, sigmaEstimatesBefore))
-
-# downscale + unsharp mask when using a graph-based method due to memory complexity
-# εδώ προφανώς θα πέσουμε στα (120, 77)
-# preprocessing // αλλαγές: αποκλιμάκωση εικόνας και αφαίρεση θορύβου
-mouseImages = [downscale_local_mean(mouseImage, (2, 2)) for mouseImage in cutImages]
-# or denoise_wavelet
-mouseImages = [unsharp_mask(mouseImage) for mouseImage in mouseImages]
-
-# Preprocessing -- στατιστικά εικόνας, αφού κάνουμε αλλαγές
-sigmaEstimatesAfter = [estimate_sigma(mouseImage) for mouseImage in mouseImages]
-meanAfter = [np.mean(mouseImage) for mouseImage in mouseImages]
-SNRAfter = list(map(lambda x, y: x / y, meanAfter, sigmaEstimatesAfter))
-#-------------- Figure: 1-figure with estimated SNR before and after preprocessing --------
-fig = plt.figure(figsize=(8, 8), constrained_layout=False)
-fig.suptitle(t='Estimated SNR', fontsize='xx-large')
-grid = fig.add_gridspec(nrows=1, ncols=1, wspace=0.1, hspace=0.1)
-
-ax = fig.add_subplot(grid[0, 0])
-ax.grid(True)
-ax.set_xticks(ticks=np.arange(0, 9, 1))
-ax.set_xticklabels(labels=sampleHours)
-ax.set_xlabel(xlabel='Sample Hours')
-
-ax.plot(np.arange(0, 9, 1), SNRBefore, 'k--', 
-        label='Cut Images')
-ax.scatter(np.arange(0, 9, 1), SNRBefore, c='black', s=10)
-ax.legend(loc='best', shadow=True, fontsize='x-large')
-
-ax.plot(np.arange(0, 9, 1), SNRAfter, 'k', 
-        label='Downscale \n + Denoise')
-ax.scatter(np.arange(0, 9, 1), SNRAfter, c='black', s=10)
-ax.legend(loc='best', shadow=True, fontsize='x-large')
-fig.add_subplot(ax)
-
+""" from src.visualization import gif_maker
+    from gif_maker import snr_print """
 
 minMouseImages, mouseMasks = 9*[], 9*[]
 for index, (cutImage, sampleHour) in enumerate(zip(mouseImages, sampleHours)):
@@ -205,7 +147,7 @@ CENTROIDS = 'k-means++'
 METHOD = ('uniform', 'default', 'ror', 'var')
 clusteringScores, n_samplesList = [], []
 
-for index, (mouseMask, mouseImage) in enumerate(zip(mouseMasks, minMouseImages)):
+for index, (mouseMask, mouseImage) in enumerate(zip(mouseMasks, minMouseImages))
     
     # μάσκα ποντικιού και ανάλυση σε διανύσματα συντεταγμένων
     maskOrdinateX, maskOrdinateY = img_to_vectors(mouseMask)
@@ -347,7 +289,7 @@ for index, (mouseMask, mouseImage) in enumerate(zip(mouseMasks, minMouseImages))
     fig.colorbar(img1, ax=ax)
     ax.set_title(label='Feature')
     ax.set_ylabel(ylabel='Intensity')
-    
+    np.ones
     ax = fig.add_subplot(grid[0, 1])
     histIntensity, hist_centersIntensity = histogram(img_as_ubyte(mouseImage) * mouseMask)
     ax.plot(hist_centersIntensity[1:], histIntensity[1:], lw=1)
@@ -604,65 +546,3 @@ for index, (mouseMask, mouseImage) in enumerate(zip(mouseMasks, minMouseImages))
     # ax.scatter(x=c, y=r, c='white', s=16)
     ax.scatter(x=skeletonOrdinateY, y=skeletonOrdinateX, c='red', s=1)
     ax.set_title(label='Eliminate Boundaries')
-    
-    
-
-# clusteringScores λίστα από 3-tuples με μήκος n_clusters 
-# πχ [(80, 12, 16), (40, 13, 17)]
-# allScores = [score for score in clusteringScores]
-# θα τα κανω scatter plot οπότε σημαίνει 3 λίστες
-# x = [80, 40] y = [12, 13] z = [16, 17] άρα τα κάνω unpack με το
-silhouette, calinski, davies = map(list, zip(*clusteringScores))
-
-meanScores = list(map(np.mean, (silhouette, calinski, davies, n_samplesList)))
-#-------------- Figure: 1-figure comparing the clustering scores with their mean values  --------
-fig = plt.figure(figsize=(8, 8), constrained_layout=False)
-
-fig.suptitle(t=f'Utilized {n_features} features \n and {n_clusters} clusters')
-grid = fig.add_gridspec(nrows=4, ncols=1, wspace=0.25, hspace=0.1)
-
-ax = fig.add_subplot(grid[0])
-ax.grid(True)
-ax.plot(np.arange(0, 9, 1), silhouette, 'k', label='Silh. Score')
-ax.scatter(np.arange(0, 9, 1), silhouette, c='black', s=10)
-ax.set_xticks(ticks=np.arange(0, 9, 1))
-ax.set_xticklabels(labels=[])
-ax.axhline(y=meanScores[0], color='r', linestyle='--', 
-           label='Mean '+"{:.2f}".format(meanScores[0]))
-ax.legend(loc='best', shadow=True, fontsize='large')
-fig.add_subplot(ax)
-
-ax = fig.add_subplot(grid[1])
-ax.grid(True)
-ax.plot(np.arange(0, 9, 1), davies, 'k', label='D-B Index')
-ax.scatter(np.arange(0, 9, 1), davies, c='black', s=10)
-ax.set_xticks(ticks=np.arange(0, 9, 1))
-ax.set_xticklabels(labels=[])
-ax.axhline(y=meanScores[2], color='r', linestyle='--', 
-           label='Mean '+"{:.2f}".format(meanScores[2]))
-ax.legend(loc='best', shadow=True, fontsize='large')
-
-fig.add_subplot(ax)
-
-ax = fig.add_subplot(grid[2])
-ax.grid(True)
-ax.plot(np.arange(0, 9, 1), calinski, 'k', label='C-H Index')
-ax.scatter(np.arange(0, 9, 1), calinski, c='black', s=10)
-ax.set_xticks(ticks=np.arange(0, 9, 1))
-ax.set_xticklabels(labels=[])
-ax.axhline(y=meanScores[1], color='r', linestyle='--', 
-           label='Mean '+"{:.2f}".format(meanScores[1]))
-ax.legend(loc='best', shadow=True, fontsize='large')
-fig.add_subplot(ax)
-
-ax = fig.add_subplot(grid[3])
-ax.grid(True)
-ax.plot(np.arange(0, 9, 1), n_samplesList, 'k', label='Mouse Area')
-ax.scatter(np.arange(0, 9, 1), n_samplesList, c='black', s=10)
-ax.set_xticks(ticks=np.arange(0, 9, 1))
-ax.set_xticklabels(labels=sampleHours)
-ax.axhline(y=meanScores[3], color='r', linestyle='--', 
-            label='Mean '+"{:.2f}".format(meanScores[3]))
-ax.legend(loc='best', shadow=True, fontsize='large')
-ax.set_xlabel(xlabel='Sample Hours')
-fig.add_subplot(ax)
