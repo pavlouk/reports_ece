ea1b77e455b3affe772b7b8fcbdd9381fa335d41
Pavlos Loukareas <ploukareas@gmail.com>
2021-04-15 13:25:10 +0300

    ✨ detect and remove image background in array

---
+++ src/features/mouse_detection.py
@@ -0,0 +1,35 @@
+import numpy as np
+from skimage.util import img_as_ubyte
+from skimage.segmentation import watershed
+from skimage.filters import sobel
+from skimage.draw import rectangle_perimeter
+
+from scipy import ndimage as ndi
+
+def mouse_detection(mouse_images):
+    object_images = []
+    for original in mouse_images:
+        bordered = original[:, 100:260]
+        # We create markers indicating the segmentation through histogram values
+        markerImage = np.zeros_like(bordered)
+        # We find markers of the background and the mouse body based on the extreme
+        # parts of the histogram of gray values.  μαρκάρω == marker, πινακιδιάζω == label
+        markerBack, markerBody = 70, 150
+        markerImage[img_as_ubyte(bordered) < markerBack] = 1
+        markerImage[img_as_ubyte(bordered) > markerBody] = 2
+        elevationMap = sobel(bordered)
+        
+        initialMaskTemp = watershed(elevationMap, markerImage)
+        # This method segments and labels the mouse individually
+        initialMask = ndi.binary_fill_holes(initialMaskTemp - 1)
+        labeledMouse, _ = ndi.label(initialMask)
+        mouseLocation = ndi.find_objects(labeledMouse)[0]
+        ro, co = rectangle_perimeter(start=(mouseLocation[0].start, mouseLocation[1].start), 
+                             end=(mouseLocation[0].stop, mouseLocation[1].stop),
+                             shape=bordered.shape)
+        initialMask[ro, co] = True
+        mouseMask = initialMask[mouseLocation]
+        mouseImage = bordered[mouseLocation]
+        cleanMouse = mouseImage * mouseMask
+        object_images.append(cleanMouse)
+    return object_images
