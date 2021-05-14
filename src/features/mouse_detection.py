5b75b9d2579944579fd0307891b44ae54533e85a
Pavlos Loukareas <ploukareas@gmail.com>
2021-05-14 16:34:10 +0300

    🐛 αλλαγή στο να μην κανει crop εικόνων

--- src/features/mouse_detection.py
+++ src/features/mouse_detection.py
@@ -3,7 +3,6 @@ from skimage.util import img_as_ubyte
 from skimage.segmentation import watershed
 from skimage.filters import sobel
 from skimage.draw import rectangle_perimeter
-from skimage.io import imread
 
 from scipy import ndimage as ndi
 
@@ -14,7 +13,7 @@ def mouse_detection(mouse_images, markerBack=70, markerBody=120):
     initial_masks = []
     mouse_locations = []
     for original in mouse_images:
-        bordered = original[:, 100:260] # (240, 160)
+        bordered = original # (240, 160)
         # We create markers indicating the segmentation through histogram values
         markerImage = np.zeros_like(bordered)
         # We find markers of the background and the mouse body based on the extreme
@@ -41,9 +40,3 @@ def mouse_detection(mouse_images, markerBack=70, markerBody=120):
         object_masks.append(mouseMask)
         object_images.append(cleanMouse)
     return object_images, object_masks, initial_masks, mouse_locations
-
-# image = imread('C:\\Users\\plouk\\Adiposer/data/raw/0h/Mouse1\\IR_2066.jpg')
-# uuu = [image]
-# new = mouse_detection(uuu)
-
-# from IPython.display import HTML 
