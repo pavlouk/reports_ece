839ecd84ab43ef6d548ec709f3c74f583eea079f
ploukareas <ploukareas@gmail.com>
2021-08-01 21:50:45 +0300

    🩹 return images with the initial dimension

--- src/features/mouse_detection.py
+++ src/features/mouse_detection.py
@@ -3,40 +3,36 @@ from skimage.util import img_as_ubyte
 from skimage.segmentation import watershed
 from skimage.filters import sobel
 from skimage.draw import rectangle_perimeter
-
 from scipy import ndimage as ndi
 
 def mouse_detection(mouse_images, markerBack=70, markerBody=120):
-    # mouse_images: (240, 320)
-    object_images = []
-    object_masks = []
-    initial_masks = []
-    mouse_locations = []
+    # mouse_images: used to be (240, 320) now are (240, 160)
+    object_images, object_masks, initial_masks, mouse_locations = [], [], [], []
     for original in mouse_images:
-        bordered = original # (240, 160)
+        bordered = original 
         # We create markers indicating the segmentation through histogram values
-        markerImage = np.zeros_like(bordered)
+        marker_image = np.zeros_like(bordered)
         # We find markers of the background and the mouse body based on the extreme
-        # parts of the histogram of gray values.  μαρκάρω == marker, πινακιδιάζω == label
+        # parts of the histogram of gray values. 
         
-        markerImage[img_as_ubyte(bordered) < markerBack] = 1
-        markerImage[img_as_ubyte(bordered) > markerBody] = 2
-        elevationMap = sobel(bordered)
+        marker_image[img_as_ubyte(bordered) < markerBack] = 1
+        marker_image[img_as_ubyte(bordered) > markerBody] = 2
+        elevation_map = sobel(bordered)
         
-        initialMaskTemp = watershed(elevationMap, markerImage)
+        initial_maskTemp = watershed(elevation_map, marker_image)
         # This method segments and labels the mouse individually
-        initialMask = ndi.binary_fill_holes(initialMaskTemp - 1)
-        initial_masks.append(initialMask)
-        labeledMouse, _ = ndi.label(initialMask)
-        mouseLocation = ndi.find_objects(labeledMouse)[0]
-        ro, co = rectangle_perimeter(start=(mouseLocation[0].start, mouseLocation[1].start), 
-                             end=(mouseLocation[0].stop, mouseLocation[1].stop),
-                             shape=bordered.shape)
-        initialMask[ro, co] = True
-        mouse_locations.append(mouseLocation)
-        mouseMask = initialMask[mouseLocation]
-        mouseImage = bordered[mouseLocation]
-        cleanMouse = mouseImage * mouseMask
-        object_masks.append(mouseMask)
-        object_images.append(cleanMouse)
-    return object_images, object_masks, initial_masks, mouse_locations
+        initial_mask = ndi.binary_fill_holes(initial_maskTemp - 1)
+        initial_masks.append(initial_mask)
+        labeled_mouse, _ = ndi.label(initial_mask)
+        mouse_location = ndi.find_objects(labeled_mouse)[0]
+        ro, co = rectangle_perimeter(start=(mouse_location[0].start, mouse_location[1].start), 
+                                    end=(mouse_location[0].stop, mouse_location[1].stop),
+                                    shape=bordered.shape)
+        initial_mask[ro, co] = True
+        mouse_locations.append(mouse_location)
+        mouse_mask = initial_mask[mouse_location]
+        mouse_image = bordered[mouse_location]
+        clean_mouse = mouse_image * mouse_mask
+        object_masks.append(mouse_mask)
+        object_images.append(clean_mouse)
+    return object_images, object_masks, initial_masks, mouse_images, mouse_locations
