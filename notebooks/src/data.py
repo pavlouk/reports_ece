5b41f327cdec9f09fab9fa306b304d114e7470cc
ploukareas <ploukareas@gmail.com>
2022-08-25 12:57:38 +0300

    moved things around

--- notebooks/src/data.py
+++ notebooks/src/data.py
@@ -23,7 +23,7 @@ PROCESSED_DIR = PROJECT_DIR / "data" / "processed"
 class FLIRImage:
     def __init__(self) -> None:
         self.id = ""
-        self.mouse_name = ""
+        self.mouse_id = ""
         self.angle = ""
         self.sample = ""
         self.ir_path = ""
@@ -36,22 +36,33 @@ class FLIRImage:
     def set_ir(self, fpath: str, as_gray=True) -> None:
         """fpath: το path της IR και η θερμική εικόνα"""
         self.ir_path = fpath
+        if not os.path.exists(fpath):
+            raise ValueError
+            self.ir_image = np.empty(shape=(320, 240))
+            return
         ir_image = imread(fpath, as_gray=as_gray)
         self.ir_image = ir_image[:, 100:260]
 
     def set_dc(self, fpath: str) -> None:
         """fpath: το path της DC και η οπτική"""
         self.dc_path = fpath
+        if not os.path.exists(fpath):
+            raise ValueError
+            self.dc_image = np.empty(shape=(320, 240))
+            return
         dc_image = imread(fpath, as_gray=True)
-        self.dc_image = dc_image[:, 100:260]
+        self.dc_image = dc_image
 
     def set_csv(self, fpath: str) -> None:
         """fpath: το path του CSV και οι θερμοκρασίες"""
         self.csv_path = fpath
+        if not os.path.exists(fpath):
+            self.csv_image = np.empty(shape=(320, 240))
+            return
         # με sep = ' ,' τότε παράγει δίστηλο frame
         df = pd.read_csv(filepath_or_buffer=fpath, sep=" ,", engine="python")
         column_names = df.columns
-        # data_column με το 'Frame 1,' έχει τα ℃ ως pd.Series
+        # 'Frame 1,' : τα ℃ ως pd.Series
         data_column = df[column_names[0]]
         first_line = data_column[0]  # string
         # 320 είναι τα ',' και στα υπόλοιπα
@@ -73,7 +84,6 @@ class FLIRImage:
     def downscale(self) -> None:
         """στο downscale_local_mean η ανάλυση γίνεται η μισή (120, 77)"""
         # self.ir_image = downscale_local_mean(self.ir_image, (2, 2))
-        # or denoise_wavelet
         self.ir_image = unsharp_mask(self.ir_image)
 
 
@@ -82,41 +92,41 @@ class ImageCollection:
 
     @classmethod
     def make_images(cls) -> None:
-        """Create global paths of the tags in the yaml file"""
-        # yaml file
+        """Create global paths from the yaml file"""
+
         module_path = os.path.abspath(PROJECT_DIR)
         with open(os.path.abspath(PROJECT_DIR / "data/multiple_views.yml")) as file:
             experiment = yaml.load(file, Loader=yaml.FullLoader)
 
         for i, hour in enumerate(experiment["samples"]):
-            for mouse_name in experiment["mouse_id"]:
-                for angle in experiment["mouse_angle"]:
+            for id in experiment["ids"]:
+                for angle in experiment["angles"]:
                     fi = FLIRImage()
-                    jpg_id = experiment[mouse_name][angle]["jpg"][i]
-                    csv_id = experiment[mouse_name][angle]["csv"][i]
-                    dc_id = experiment[mouse_name][angle]["dc"][i]
+                    jpg_id = experiment[id][angle]["ir"][i]
+                    csv_id = experiment[id][angle]["csv"][i]
+                    dc_id = experiment[id][angle]["dc"][i]
                     fi.id = jpg_id
-                    fi.mouse_name = mouse_name
+                    fi.mouse_id = id
                     fi.angle = angle
                     fi.sample = hour
-                    fi.set_ir(
-                        fpath=f"{module_path}/data/raw/{hour}/{mouse_name}/{jpg_id}"
-                    )
-                    fi.set_csv(f"{module_path}/data/raw/{hour}/{mouse_name}/{csv_id}")
-                    fi.set_dc(f"{module_path}/data/raw/{hour}/{mouse_name}/{dc_id}")
+                    fi.set_ir(fpath=f"{module_path}/data/raw/{hour}/{id}/{jpg_id}")
+                    fi.set_csv(fpath=f"{module_path}/data/raw/{hour}/{id}/{csv_id}")
+                    fi.set_dc(fpath=f"{module_path}/data/raw/{hour}/{id}/{dc_id}")
                     cls.flir_images.append(fi)
 
     @classmethod
     def get_by_id(cls, id) -> FLIRImage:
+        empty_image = FLIRImage()
         for flir_image in cls.flir_images:
             if flir_image.id == id:
                 return flir_image
+        return empty_image
 
     @classmethod
     def get_by_mouse_name(cls, mouse_id) -> List[FLIRImage]:
         flir_images = []
         for flir_image in cls.flir_images:
-            if mouse_id == flir_image.mouse_name:
+            if mouse_id == flir_image.mouse_id:
                 flir_images.append(flir_image)
         return flir_images
 
@@ -134,11 +144,4 @@ class ImageCollection:
         for flir_image in cls.flir_images:
             if angle == flir_image.angle:
                 flir_images.append(flir_image)
-
-
-image_collection = ImageCollection()
-image_collection.make_images()
-
-for image in image_collection.flir_images:
-    image.csv_image
-    image.csv_path
+        return flir_images
