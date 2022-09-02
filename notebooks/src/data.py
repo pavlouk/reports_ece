59a2bcede0da9bc70870d87ec2d1c7d5b1dd174a
ploukareas <ploukareas@gmail.com>
2022-09-02 10:31:52 +0300

    tested data and detection

--- notebooks/src/data.py
+++ notebooks/src/data.py
@@ -1,58 +1,51 @@
 import os
 from pathlib import Path
-from typing import List, Tuple
+from typing import List
 
+import cv2 as cv
 import numpy as np
 import pandas as pd
 import yaml
-# from skimage.feature import downscale_local_mean
-from skimage.filters import unsharp_mask
-from skimage.io import imread
 
-HERE = Path(__file__)  # ~/Adiposer/src/data/data_utils.py
-SRC_DIR = HERE.parent
-PROJECT_DIR = SRC_DIR.parent
-RAW_DIR = PROJECT_DIR / "data" / "raw"
-INTERIM_DIR = PROJECT_DIR / "data" / "interim"
-PROCESSED_DIR = PROJECT_DIR / "data" / "processed"
-project_dir = os.path.abspath(PROJECT_DIR)
+HERE = Path(__file__)
+ADIPOSER_DIR = HERE.parent.parent.parent
+RAW_DIR = ADIPOSER_DIR / "data" / "raw"
+INTERIM_DIR = ADIPOSER_DIR / "data" / "interim"
+PROCESSED_DIR = ADIPOSER_DIR / "data" / "processed"
+project_dir = os.path.abspath(ADIPOSER_DIR)
 
 
 class FLIRImage:
     def __init__(self) -> None:
-        self.mouse_id = ""
+        self.mouse = ""
         self.angle = ""
         self.sample = ""
         self.ir_id = ""
         self.dc_id = ""
         self.csv_id = ""
-        self.ir_image = np.ndarray(shape=(320, 240))
-        self.dc_image = np.ndarray(shape=(320, 240))
-        self.csv_image = np.ndarray(shape=(320, 240))
+        self.ir_image = np.zeros(shape=(320, 240))
+        self.dc_image = np.zeros(shape=(2048, 1536))
+        self.csv_image = np.zeros(shape=(320, 240))
 
     def set_ir(self) -> None:
-        """fpath: το path της IR και η θερμική εικόνα"""
-        ir_path = f"{project_dir}/data/raw/{self.sample}/{self.mouse_id}/{self.ir_id}"
+        ir_path = f"{project_dir}/data/raw/{self.sample}/{self.mouse}/{self.ir_id}"
         if not os.path.exists(ir_path):
-            self.ir_image = np.zeros(shape=(320, 240))
+            self.ir_id = "NOT FOUND"
             return
-        ir_image = imread(ir_path, as_gray=True)
+        ir_image = cv.imread(ir_path, flags=cv.IMREAD_GRAYSCALE)
         self.ir_image = ir_image[:, 100:260]
 
     def set_dc(self) -> None:
-        """fpath: το path της DC και η οπτική"""
-        dc_path = f"{project_dir}/data/raw/{self.sample}/{self.mouse_id}/{self.dc_id}"
+        dc_path = f"{project_dir}/data/raw/{self.sample}/{self.mouse}/{self.dc_id}"
         if not os.path.exists(dc_path):
-            self.dc_image = np.empty(shape=(320, 240))
+            self.dc_id = "NOT FOUND"
             return
-        dc_image = imread(dc_path, as_gray=True)
-        self.dc_image = dc_image
+        self.dc_image = cv.imread(dc_path, flags=cv.IMREAD_GRAYSCALE)
 
     def set_csv(self) -> None:
-        """fpath: το path του CSV και οι θερμοκρασίες"""
-        csv_path = f"{project_dir}/data/raw/{self.sample}/{self.mouse_id}/{self.csv_id}"
+        csv_path = f"{project_dir}/data/raw/{self.sample}/{self.mouse}/{self.csv_id}"
         if not os.path.exists(csv_path):
-            self.csv_image = np.zeros(shape=(320, 240))
+            self.csv_id = "NOT FOUND"
             return
         # με sep = ' ,' τότε παράγει δίστηλο frame
         df = pd.read_csv(filepath_or_buffer=csv_path, sep=" ,", engine="python")
@@ -72,41 +65,44 @@ class FLIRImage:
 
 
 class ImageCollection:
-
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self, *args) -> None:
         self.image_collection: List[FLIRImage] = []
-        if args == "all":
-            return 
-        # load csv sample file and let pandas do filtering 
-        with open(os.path.abspath(PROJECT_DIR / "data/multiple_views.yml")) as file:
-            experiment = yaml.load(file, Loader=yaml.FullLoader)
-        df = pd.DataFrame()
-        for i, sample in enumerate(experiment["samples"]):
-            for mouse_id in experiment["ids"]:
-                for angle in experiment["angles"]:
-                    fi = FLIRImage()
-                    fi.mouse_id = mouse_id
-                    fi.angle = angle
-                    fi.sample = sample
-                    fi.ir_id = experiment[mouse_id][angle]["ir"][i]
-                    fi.csv_id = experiment[mouse_id][angle]["csv"][i]
-                    fi.dc_id = experiment[mouse_id][angle]["dc"][i]
-                    fi.set_ir()
-                    fi.set_csv()
-                    fi.set_dc()
-                    self.image_collection.append(fi)
+        if args[0] == "all":
+            return
+        if args[0] == "IR_2060.jpg":
+            fi = FLIRImage()
+            fi.mouse = "mouse_1"
+            fi.angle = "posterior"
+            fi.sample = "0h"
+            fi.ir_id = "IR_2060.jpg"
+            fi.csv_id = "CSV_2060.jpg"
+            fi.dc_id = "DC_2061.jpg"
+            fi.set_ir()
+            fi.set_csv()
+            fi.set_dc()
+            self.image_collection.append(fi)
+        # load csv sample file and let pandas do filtering
+        # with open(os.path.abspath(PROJECT_DIR / "data/multiple_views.yml")) as file:
+        #     experiment = yaml.load(file, Loader=yaml.FullLoader)
+        # # for i, sample in enumerate(experiment["samples"]):
+        #     for mouse_id in experiment["ids"]:
+        #         for angle in experiment["angles"]:
 
-    def get_by_id(self, id) -> FLIRImage:
-        empty_image = FLIRImage()
+    def get_single_image(self, id) -> FLIRImage:
+        default_image = FLIRImage()
         for flir_image in self.image_collection:
-            if flir_image.ir_id == id or flir_image.dc_id == id or flir_image.csv_id == id:
+            if (
+                flir_image.ir_id == id
+                or flir_image.dc_id == id
+                or flir_image.csv_id == id
+            ):
                 return flir_image
-        return empty_image
+        return default_image
 
-    def get_by_mouse_name(self, mouse_id) -> List[FLIRImage]:
+    def get_by_mouse_name(self, mouse) -> List[FLIRImage]:
         flir_images = []
         for flir_image in self.image_collection:
-            if mouse_id == flir_image.mouse_id:
+            if mouse == flir_image.mouse:
                 flir_images.append(flir_image)
         return flir_images
 
