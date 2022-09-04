8b0f6fd09f0b972b1ab30f5747e249d17ab3ae8d
ploukareas <ploukareas@gmail.com>
2022-09-04 09:30:19 +0300

    tested data utilities

--- notebooks/src/test/test_data.py
+++ notebooks/src/test/test_data.py
@@ -12,6 +12,12 @@ class TestData(unittest.TestCase):
         ic = ImageCollection("IR_2060.jpg")
         single = ic.get_single_image(id="IR_2060.jpg")
         self.assertEqual(single.ir_id, "IR_2060.jpg")
+        self.assertEqual(single.csv_id, "CSV_2060.csv")
+        self.assertEqual(single.dc_id, "DC_2061.jpg")
+
+    def test_csv_load(self):
+        ic = ImageCollection("all")
+        self.assertEqual(len(ic.df), 180)
 
 
 if __name__ == "__main__":
