6a2e37f8bbc95729523e4045a79f8b6507e9b3f9
ploukareas <ploukareas@gmail.com>
2022-09-06 14:26:07 +0300

    deleted csv test

--- notebooks/src/test/test_data.py
+++ notebooks/src/test/test_data.py
@@ -5,19 +5,15 @@ from ..data import ImageCollection
 
 class TestData(unittest.TestCase):
     def test_count(self):
-        ic = ImageCollection("IR_2060.jpg")
-        self.assertEqual(len(ic.image_collection), 1)
+        ic = ImageCollection()
+        self.assertEqual(len(ic.image_collection), 180)
 
     def test_id(self):
-        ic = ImageCollection("IR_2060.jpg")
+        ic = ImageCollection(ir_id="IR_2060.jpg")
         single = ic.get_single_image(id="IR_2060.jpg")
         self.assertEqual(single.ir_id, "IR_2060.jpg")
         self.assertEqual(single.csv_id, "CSV_2060.csv")
         self.assertEqual(single.dc_id, "DC_2061.jpg")
-
-    def test_csv_load(self):
-        ic = ImageCollection("all")
-        self.assertEqual(len(ic.df), 180)
 
 
 if __name__ == "__main__":
