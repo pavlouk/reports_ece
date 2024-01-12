7f5cbdf578d7693a63020de60629a3c69c492053
Pavlos Loukareas <loukareas@vermantia.com>
2024-01-12 14:45:16 +0200

    updated table creation

--- bus_app/database_functions/model_based/charge_functions.py
+++ bus_app/database_functions/model_based/charge_functions.py
@@ -1,4 +1,4 @@
-from bus_app.sql.create_tables import CREATE_CHARGE_TABLE
+from bus_app.sql.create_tables import CREATE_PURCHASE_TABLE
 from bus_app.sql.insert_tables import INSERT_CHARGE
 from datetime import datetime
 
@@ -6,7 +6,7 @@ class ChargeHelp:
     def __init__(self, cursor, connection):
         self.cursor = cursor
         self.connection = connection
-        self.cursor.executescript(CREATE_CHARGE_TABLE)
+        self.cursor.executescript(CREATE_PURCHASE_TABLE)
 
     def add_charge(self, tickets, pay, card_id, category_id):
         with self.connection:
