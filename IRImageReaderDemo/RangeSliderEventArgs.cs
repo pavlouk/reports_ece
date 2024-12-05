e4bcc90b5fa3eb51c96122920f8e68dcccea7792
ploukareas <ploukareas@gmail.com>
2024-12-05 19:48:08 +0200

    rename: adipose tissue detector directory

--- IRImageReaderDemo/RangeSliderEventArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace IRImageReaderDemo
-{
-    public class RangeSliderEventArgs : EventArgs
-    {
-        new public static readonly RangeSliderEventArgs Empty = new RangeSliderEventArgs();
-    }
-}
